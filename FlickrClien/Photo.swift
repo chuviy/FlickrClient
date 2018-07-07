55681bce6de461c0eb87293970e8192646bb7d3c
Aleksey3a <aleksey3a@macbook-pro-alexey.local>
2018-07-07 03:21:40 +0300

    Release#1

--- FlickrClien/Photo.swift
+++ FlickrClien/Photo.swift
@@ -12,15 +12,19 @@ import SwiftyJSON
 struct Photo {
     var bigImageURL: String
     var smallImageURL: String
+ //   var geoTag:String
     
     init?(json: JSON) {
         guard let urlQ = json["url_q"].string,
-            let urlZ = json["url_z"].string else {
+              let urlZ = json["url_z"].string
+           //   let geo = json["geo"].string
+            else {
                 return nil
         }
         
         self.bigImageURL = urlZ
         self.smallImageURL = urlQ
+      //  self.geoTag = geo
     }
     
 }
