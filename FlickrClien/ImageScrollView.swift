3cfc108eecf9540a1345092d4c35cd5a2f65aeaa
Aleksey Antokhin <antokhin.aleksey@gmail.com>
2019-10-09 22:55:35 +0300

    imageZoomView _ class ImageScrollView release

--- FlickrClien/ImageScrollView.swift
+++ FlickrClien/ImageScrollView.swift
@@ -12,6 +12,12 @@ class ImageScrollView: UIScrollView, UIScrollViewDelegate {
 
     var imageZoomView: UIImageView!
     
+    lazy var zoomingTap: UITapGestureRecognizer = {
+        let zoomingTap = UITapGestureRecognizer (target: self, action: #selector(handleZoomingTap))
+        zoomingTap.numberOfTapsRequired = 2
+        return zoomingTap
+    }()
+    
     override init(frame: CGRect) {  // Переопределяем инициализатор
         super.init(frame: frame)
         
@@ -35,22 +41,22 @@ class ImageScrollView: UIScrollView, UIScrollViewDelegate {
         configureFor(imageSize: image.size)
     }
  
-    func configureFor(imageSize: CGSize) {   // CGSize - размер
+    func configureFor(imageSize: CGSize) {   // формируем базовые настройки (CGSize - это здесь размер)
         self.contentSize = imageSize // вызываем у объекта класса ImageScrollView сво-во contentSize и назначаем размеры картинки
       setCurrentMaxandMinZoomScale()
       self.zoomScale = self.minimumZoomScale // вписываем картинку пропорционально экрану
-        
-//        self.minimumZoomScale = 0.1
-//        self.maximumZoomScale = 5
+      
+        self.imageZoomView.addGestureRecognizer(self.zoomingTap) // регистрируем жест zoomingTap
+        self.imageZoomView.isUserInteractionEnabled = true
     }
     
-    override func layoutSubviews() { // отслеживает taps
+    override func layoutSubviews() { // срабатывает когда любым образом изменяется картинка
         super.layoutSubviews()
         
         self.centerImage() // вызов функции для оцентровки изображения
     }
     
-    func setCurrentMaxandMinZoomScale() { // описываем логику изменения масштаба картинки
+    func setCurrentMaxandMinZoomScale() { // Высчитываем min max zoomScale (описываем логику изменения масштаба картинки)
         let boundSize = self.bounds.size // фиксируем рамки
         let imageSize = imageZoomView.bounds.size // фиксируем размеры изображения
         
@@ -69,11 +75,10 @@ class ImageScrollView: UIScrollView, UIScrollViewDelegate {
         if minScale >= 0.5 {
             maxScale = max(1.0, minScale)
         }
-        
         self.minimumZoomScale = minScale
         self.maximumZoomScale = maxScale
     }
-    func centerImage() {
+    func centerImage() { // центрируем наше изображение
         let boundSize = self.bounds.size // фиксируем рамки
         var frameToCenter = imageZoomView.frame ?? CGRect.zero // фиксируем фрейм до центра + default value '?? CGRect.zero'
         
@@ -88,6 +93,39 @@ class ImageScrollView: UIScrollView, UIScrollViewDelegate {
             frameToCenter.origin.y = 0
         }
         imageZoomView.frame = frameToCenter // Присваеваем новый frame который посчитали
+    }
+    
+    // MARK: - Gesture
+    @objc func handleZoomingTap(sender: UITapGestureRecognizer) {
+        let location = sender.location(in: sender.view) // ловим то место на экране куда нажали
+        self.zoom(point: location, animated: true)
+    }
+    
+    func zoom(point: CGPoint, animated: Bool) {
+        let currentScale = self.zoomScale
+        let minScale = self.minimumZoomScale
+        let maxScale = self.maximumZoomScale
+        if (minScale == maxScale && minScale > 1) {
+            return
+        }
+        
+        let toScale = maxScale
+        let finalScale = (currentScale == minScale) ? toScale : minScale
+        let zoomRect = self.zoomRect(scale: finalScale, center: point)
+        self.zoom(to: zoomRect, animated: animated) // вызываем нативную(не свою) функцию (default)
+    }
+    
+    func zoomRect(scale: CGFloat, center: CGPoint) -> CGRect {
+        var zoomRect = CGRect.zero
+        let bounce = self.bounds // границы
+        
+        zoomRect.size.width = bounds.size.width / scale
+        zoomRect.size.height = bounds.size.height / scale
+        
+        zoomRect.origin.x = center.x - (zoomRect.size.width / 2)
+        zoomRect.origin.y = center.y - (zoomRect.size.height / 2)
+        return zoomRect
+
     }
     
     // MARK: - UIScrollViewDelegate
